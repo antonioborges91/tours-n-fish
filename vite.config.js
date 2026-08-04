aaf8fe0832f7163cd9b9bbaf447456207a06ef0a
antonioborges91 <javborges@gmail.com>
2026-08-04 17:19:02 +0000

    Add bilingual support for home page11

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,11 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         tailwindcss(),
-
         laravel({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
+                'resources/js/admin.js',
             ],
             refresh: true,
         }),

4cf08c90cae59ac58346b00f00481ad64550af2d
antonioborges91 <javborges@gmail.com>
2026-08-05 10:23:30 +0000

    050/2026

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,7 +14,11 @@
 
     <meta name="author" content="Tours N Fish">
 
-    <link rel="icon" href="{{ asset('favicon.ico') }}">
+    <!-- Favicons -->
+    <link rel="icon" href="{{ asset('favicon.ico') }}" sizes="any">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon-16x16.png') }}">
+    <link rel="apple-touch-icon" href="{{ asset('apple-touch-icon.png') }}">
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -30,13 +34,13 @@
     @include('components.navbar.index')
 
     <main>
-
         @yield('content')
-
     </main>
 
     @include('components.footer.index')
-@stack('scripts')
+
+    @stack('scripts')
+
 </body>
 
 </html>
